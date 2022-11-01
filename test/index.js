91f95e91c822328c1eeeb9473b8a3a7eabeca985
reco <1000copy@gmail.com>
2022-11-01 15:47:28 +0800

    autoload available

--- test/index.js
+++ test/index.js
@@ -1,121 +1,22 @@
-/* Call Examples
-    var rest = {
-        resource:"person",
-        action:"select",
-        params:{fields:['name','age'],condition:{name:"lcj"}}
-    }
-    var obj = {name:"person",fields:[{name:"name",type:"text primary key"},{name:"age",type:"text"}]}    
-    var restresult = [{name:'lcj',age:'18'}]
-    var restresult90 = [{name:'lcj',age:'90'}]
-    var restInsert = {
-            resource:"person",
-            action:"insert",
-            params:{name:"lcj",age:"18"}
-        }
-    var restUpdate = {
-            resource:"person",
-            action:"update",
-            params:{value:{age:"90"},condition:"name='lcj'"}
-        }
-    var restClear = {
-            resource:"person",
-            action:"clear"        
-        }
- */
-var jd = require('../lib/.')
-var t = require("assert")
-t(jd.register)
-t(jd.dispatch)
-class Obj1{
-    get name(){return 'obj1'}
-    action1(){
-        result = "action1 of obj1 is here"
-        // console.log(result)
-        return result;
-    }
-    action2(params){
-        return params;
-    }
-}
-jd.register(new Obj1())
-var result = jd.dispatch({resource:"obj1",action:"action1",params:{}})
-// console.log(result)
-t( "action1 of obj1 is here"==result.data)
-// console.log(result)
-jd.register(new Obj1())
-var result = jd.dispatch({resource:"obj1",action:"action2",params:{echo:1}})
-t(1==result.data.echo)
-var Obj2 ={
-    name:"reco",
-    action(){
-        return 'obj2'
-    }
-}
-jd.register(Obj2)
-var r2 = jd.dispatch({resource:'reco',action:'action',params:{}})
-// console.log(r2)
-t(r2.data=='obj2')
-var r2 = jd.dispatch({resource:'reco',action:'noaction'})
-t(r2.error == 'no action named noaction')
-var Obj3 ={
-    name:"reco3",
-    action(params){
-        return params.name
-    }
-}
-jd.register(Obj3)
-var r2 = jd.dispatch({resource:'reco3',action:'action',params:{name:'reco'}})
-// console.log(r2)
-t(r2.data=='reco')
-var r2 = jd.dispatch({resource:'reco3',action:'action'})
-// console.log(r2)
-t(r2.error == "Cannot read properties of undefined (reading 'name')")
-var jd = require('../.')
-// console.log(jd)
-t(jd.register)
-t(jd.dispatch)
-t({a:1}!={a:1})
-function check(left,right,msg){
-  if(!deepEqual(left,right)){
-    if(msg)
-    console.log(msg)
-    console.trace(left,' not equal to ',right)
-    // console.log(right)
-    process.exit(1)
-  }
-}
-var deepEqual = function (x, y) {
-    if (x === y) {
-      return true;
-    }
-    else if ((typeof x == "object" && x != null) && (typeof y == "object" && y != null)) {
-      if (Object.keys(x).length != Object.keys(y).length)
-        return false;
-  
-      for (var prop in x) {
-        if (y.hasOwnProperty(prop))
-        {  
-          if (! deepEqual(x[prop], y[prop]))
-            return false;
-        }
-        else
-          return false;
-      }
-      
-      return true;
-    }
-    else 
-      return false;
-  }
-check(1,2)
-check({a:1},{a:1,b:2},'haha')
+
+var check = require('../lib/check.js').check
+check(1,1)
+check({a:1},{a:1},'haha')
 // check({a:1},{a:1,b:2},'haha')
 check([1,2],[1,2],'hh')
 // t([1]===[1])
 var y = [1,2,3]
-console.log(Object.keys(y).length)
-console.log(typeof y)
-for(var p in y){
-    console.log(p)
-}
-console.log(Object.keys(y))
+check(1,1,'ab')
+check(1,1,'ab1')
+check(3,Object.keys(y).length)
+// read & write file stream
+// var fs = require('fs')
+// var fss = fs.createWriteStream('a.md');
+// fss.write("Hi,");
+// fss.write("Thank You.");
+// fss.end();
+// var result;
+// var chunks = [];
+// var fss = fs.createReadStream('a.md');
+// fss.on("data", chunk => chunks.push(Buffer.from(chunk)));
+// fss.on("end", () => {console.log(chunks);result=Buffer.concat(chunks).toString("utf-8");check(result,'Hi,Thank You.');chunks=[]});
---
+++ test/jdOldStyle.js
@@ -0,0 +1,72 @@
+/* Call Examples
+    var rest = {
+        resource:"person",
+        action:"select",
+        params:{fields:['name','age'],condition:{name:"lcj"}}
+    }
+    var obj = {name:"person",fields:[{name:"name",type:"text primary key"},{name:"age",type:"text"}]}    
+    var restresult = [{name:'lcj',age:'18'}]
+    var restresult90 = [{name:'lcj',age:'90'}]
+    var restInsert = {
+            resource:"person",
+            action:"insert",
+            params:{name:"lcj",age:"18"}
+        }
+    var restUpdate = {
+            resource:"person",
+            action:"update",
+            params:{value:{age:"90"},condition:"name='lcj'"}
+        }
+    var restClear = {
+            resource:"person",
+            action:"clear"        
+        }
+ */
+var jd = require('../lib/.')
+var t = require("assert")
+t(jd.register)
+t(jd.dispatch)
+class Obj1{
+    get name(){return 'obj1'}
+    action1(){
+        result = "action1 of obj1 is here"
+        // console.log(result)
+        return result;
+    }
+    action2(params){
+        return params;
+    }
+}
+jd.register(new Obj1())
+var result = jd.dispatch({resource:"obj1",action:"action1",params:{}})
+// console.log(result)
+t( "action1 of obj1 is here"==result.data)
+// console.log(result)
+jd.register(new Obj1())
+var result = jd.dispatch({resource:"obj1",action:"action2",params:{echo:1}})
+t(1==result.data.echo)
+var Obj2 ={
+    name:"reco",
+    action(){
+        return 'obj2'
+    }
+}
+jd.register(Obj2)
+var r2 = jd.dispatch({resource:'reco',action:'action',params:{}})
+// console.log(r2)
+t(r2.data=='obj2')
+var r2 = jd.dispatch({resource:'reco',action:'noaction'})
+t(r2.error == 'no action named noaction')
+var Obj3 ={
+    name:"reco3",
+    action(params){
+        return params.name
+    }
+}
+jd.register(Obj3)
+var r2 = jd.dispatch({resource:'reco3',action:'action',params:{name:'reco'}})
+// console.log(r2)
+t(r2.data=='reco')
+var r2 = jd.dispatch({resource:'reco3',action:'action'})
+// console.log(r2)
+t(r2.error == "Cannot read properties of undefined (reading 'name')")
