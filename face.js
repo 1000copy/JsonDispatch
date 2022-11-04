91dac43548e1acaeda8959b34877984fc5e8c57f
reco <1000copy@gmail.com>
2022-11-04 11:02:12 +0800

    pass through the road to autoload

---
+++ face.js
@@ -0,0 +1,2 @@
+const { check } = require('./lib/check')
+check(1,1,'3');
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const { check } = require('./lib/check')
-check(1,1,'3')
-const { start,stop } = require('./lib/httpserver.js')
+
+const hs= require('./lib/httpserver.js')
 var ws  = require('./lib/ws.js')
 var timerid;
 var api_ ;
 module.exports.start = ()=>{
     
-    start()
+    hs.start()
     let sws = ws.start
     sws({
         port: 8095,
@@ -24,23 +23,25 @@ module.exports.start = ()=>{
             
         }
     });
-    
+    require('./face.js')
 };
 module.exports.stop = ()=>{
     clearInterval(timerid)
-    stop()
+    hs.stop()
     ws.stop()
 };
-module.exports.reload = ()=>{
-    if(process.env['reload']){
-        process.env['reload'] =false
-        return
-    }
+module.exports.reload = (clearscreen)=>{
+    // if(process.env['reload']){
+    //     process.env['reload'] =false
+    //     return
+    // }
     if(api_){
         // console.log('notify client reload')
         api_.sendText('reload')
         process.env['reload'] = true
     }
+    console.log('reload')
+    require('./face.js')
 };
 
 
